8861f581e9caa8274b8a61539ee3c121e6eba0ff
Gabriel Santos Barbosa <0077530@IFMG.EDU.BR>
2023-09-14 09:33:25 -0300

    Exercicio 3

---
+++ exercicio3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <windows.h>
+ 
+using namespace std;
+ 
+int main() {
+  UINT CPAGE_UTF8 = 65001;
+  UINT CPAGE_DEFAULT = GetConsoleOutputCP();
+  SetConsoleOutputCP(CPAGE_UTF8);
+ 
+  int soma[3][3],aux=0;
+
+  for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+      cin >> soma[i][j];
+      aux+=soma[i][j];
+    }
+    cout << aux << endl;
+    aux=0;
+  }
+ 
+ 
+  cout << endl << endl;
+  return 0;
+}
